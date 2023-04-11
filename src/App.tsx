d7da54b5c0d202f27adad397694fae27a7af4a3f
ymgmcmc <72509071+naoya-horai@users.noreply.github.com>
2023-04-12 00:11:57 +0900

    Revert "resetした"
    This reverts commit 5ee163d92dcd71667b843d4050f0b3b58383dd41.
    
    # Conflicts:
    #	package-lock.json

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,9 @@ function App() {
         >
           Learn React
         </a>
+        <button className="bg_red-500 text-white font bold py-2 px-4 ">
+          hello tailwind
+        </button>
       </header>
     </div>
   );
