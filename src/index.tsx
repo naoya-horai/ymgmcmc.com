92b39abe37b64ac989ea013fca0bb58e7203520f
naoyahorai <0911horai@gmail.com>
2023-05-22 16:04:35 +0900

    app.tsxの中身を空にしてみる2

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//import App from './App';
 import Particles from 'react-tsparticles';
 import reportWebVitals from './reportWebVitals';
 
