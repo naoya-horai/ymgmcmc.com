c76dbff97edc419a0c60d15594cd41b886946978
naoyahorai <0911horai@gmail.com>
2023-05-25 00:37:28 +0900

    videをローカルに、レスポンジブ対応、bodyにリンク追加、使わないファイルをarchivesに避難

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 
 import logo from './logo.svg';
 
-import ParticleBackground from './particlebackground';
-import Vimeo from '@u-wave/react-vimeo'
+import ParticleBackground from './archives/particlebackground';
+
 import Footer from './footer'
+import video from "./Asset/woodmirror_Unity.mp4"
+
 export default function App() {
   return (
     <div className="font-body" >    
+      <div className='relative flex items-center h-screen overflow-hidden'>
+      <video src = {video} autoPlay loop muted playsInline className='absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 min-w-full max-w-none min-h-full max-h-none'/>
+      <body className=' absolute top-2/3 left-1/2 -translate-x-1/2 justify-center'>
+        <nav className="flex flex-wrap justify-center gap-x-4 gap-y-2 md:justify-start md:gap-6 text-xl">
+            <a href="#" className="text-white transition duration-100 hover:text-indigo-500 active:text-indigo-600">About</a>
+            <a href="#" className="text-white transition duration-100 hover:text-indigo-500 active:text-indigo-600">Works</a>
+            <a href="https://docs.ymgmcmc.com" className="text-white transition duration-100 hover:text-indigo-500 active:text-indigo-600">Blog</a>
+            <a href="#" className="text-white transition duration-100 hover:text-indigo-500 active:text-indigo-600">Contact</a>
+        </nav>
+      </body>
+        
+      </div>
       
-      <Vimeo video = "805782314" autoplay background responsive loop/>
       <header className="absolute top-0" hidden>
         <img src={logo} className="App-logo" alt="logo"/>
       </header> 
--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,5 +1,8 @@
 import { ReportHandler } from 'web-vitals';
 
+
+
+
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
@@ -11,5 +14,6 @@ const reportWebVitals = (onPerfEntry?: ReportHandler) => {
     });
   }
 };
+
 
 export default reportWebVitals;
