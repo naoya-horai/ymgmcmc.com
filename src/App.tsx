8da455f4e4377d50c8ea66b636f76aebdf8b0110
naoyahorai <0911horai@gmail.com>
2023-05-25 12:23:25 +0900

    change logo size

--- src/App.tsx
+++ src/App.tsx
@@ -14,10 +14,10 @@ export default function App() {
       
       </video>
       <div className='absolute min-w-full max-w-none min-h-full max-h-none pattern-rectangles pattern-black pattern-bg-transparent pattern-opacity-60 pattern-size-1 '/> 
-      <img src={logo} className="w-30 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 justify-center" alt="logo"/>
+      <img src={logo} className="w-40 md:w-60 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 justify-center" alt="logo"/>
       <body className=' absolute top-2/3 left-1/2 -translate-x-1/2 justify-center'>
       
-        <nav className="flex md:flex-row flex-col justify-center gap-x-4 gap-y-2 md:justify-start md:gap-6 text-sm drop-shadow-sm">
+        <nav className="flex md:flex-row flex-col justify-center gap-x-4 gap-y-2 md:justify-start md:gap-6 text-md drop-shadow-sm">
             <a href="#" className="text-white transition duration-100 hover:text-indigo-500 active:text-indigo-600">About</a>
             <a href="#" className="text-white transition duration-100 hover:text-indigo-500 active:text-indigo-600">Works</a>
             <a href="https://docs.ymgmcmc.com" className="text-white transition duration-100 hover:text-indigo-500 active:text-indigo-600">Blog</a>
